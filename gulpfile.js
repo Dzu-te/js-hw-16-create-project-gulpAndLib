c626a2122f5b4473bb1af6142b07f5aa7dc6b6dd
Yurii Pochapskiy <pochapskiy1983@gmail.com>
2025-03-18 22:33:36 +0200

    add: gulp and modules

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const { src, dest, task, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('cssnano');
+const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
+const browserSync = require('browser-sync').create();
+
+const plugins = [cssnano({ preset: 'default' })];
+
+
+function scssMin() {
+  return src('./src/**/*.scss', { sourcemaps: true })
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('assets/styles'), { sourcemaps: true });
+}
+
+function syncInit() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+}
+
+async function sync() {
+  browserSync.reload()
+}
+
+function watchFiles() {
+  syncInit()
+  scssMin()
+  watch('./src/**/*.scss', scssMin)
+  watch('./*.html', sync)
+  watch('./src/**/*.js', sync)
+}
+
+
+task('watch', watchFiles);
---
+++ src/main.js
@@ -0,0 +1,4 @@
+const helloElement = document.createElement('div');
+helloElement.innerHTML = 'Hello, World!';
+document.body.appendChild(helloElement);
+
